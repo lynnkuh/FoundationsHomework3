d52b82a8814f2e2666d5964eb3b0620043f5daf2
Regular User <lynnk@Lynns-MacBook-Pro.local>
2015-09-29 09:20:01 -0700

    IOSFoundationsHomework3

---
+++ IOSFoundationHomework3/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  IOSFoundationHomework3
+//
+//  Created by Regular User on 9/28/15.
+//  Copyright © 2015 Lynn Kuhlman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var array1 = [0, 1, 2, 3, 4, 5]
+    var array2 = [100, 101, 102, 103, 104, 105]
+    var counter = 0
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    @IBOutlet weak var ArrayLabel1: UILabel!
+    
+  
+    @IBAction func ArrayButton1Pressed(sender: AnyObject) {
+        let temp = array1[counter]
+        let lastValue = array1.last
+        ArrayLabel1.text = "\(temp)"
+        
+        
+        
+        if  temp == lastValue {
+            counter = 0
+        }
+        
+        counter++
+    }
+    
+    
+    @IBOutlet weak var ArrayLabel2: UILabel!
+    
+    @IBAction func ArrayButton2Pressed(sender: AnyObject) {
+        
+        let temp = array2[counter]
+        let lastValue = array2.last
+        ArrayLabel2.text = "\(temp)"
+        
+        
+        
+        if  temp == lastValue {
+            counter = 0
+        }
+        
+        counter++
+    }
+
+}
